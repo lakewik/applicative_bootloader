4ccb64a5fefc190ff620c666a861923f3514c197
lakewik <wikjezioro@op.pl>
2025-04-01 07:41:15 +0200

    Add missing compile scripts

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
+
+
+cairo-compile --cairo_path=cairo-lang/src applicative_bootloader_multiple_nodes.cairo --output applicative_bootloader_multiple_nodes.compiled.json --no_debug_info --proof_mode
+cairo-compile --cairo_path=cairo-lang/src fibbonaci_aggregator_multiple_nodes.cairo --output fibbonaci_aggregator_multiple_nodes.compiled.json --no_debug_info
+cairo-compile --cairo_path=cairo-lang/src default_aggregator.cairo --output default_aggregator.compiled.json --no_debug_info
 cairo-compile --cairo_path=cairo-lang/src node.cairo --output node.compiled.json --no_debug_info --proof_mode
 cairo-compile --cairo_path=cairo-lang/src aggregator.cairo --output aggregator.compiled.json --no_debug_info
 cairo-compile --cairo_path=cairo-lang/src applicative_bootloader.cairo --output applicative_bootloader.compiled.json --no_debug_info --proof_mode
